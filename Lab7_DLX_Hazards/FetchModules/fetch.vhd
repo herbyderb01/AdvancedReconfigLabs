8e64bbe782d5b915d44995c5d5f1a948e9b458ea
deaf-user <nate.walker.timp@gmail.com>
2026-03-03 14:42:58 -0700

    working on stupid processor dumb dumb idiot(the processor)

--- Lab7_DLX_Hazards/FetchModules/fetch.vhd
+++ Lab7_DLX_Hazards/FetchModules/fetch.vhd
@@ -103,6 +103,7 @@ begin
 
 end architecture component_list;
 
+--single dummy instruction to fix bug
 -- DEPTH = 1024; 
 -- WIDTH = 32;
 -- ADDRESS_RADIX = HEX;
@@ -113,33 +114,33 @@ end architecture component_list;
 -- 000 : 04200001; --LW    R1, n(R0)
 -- 001 : 0CA10000; --ADD   R5, R1, R0
 
--- 002 : ACA00012; --BEQZ  R5, 039   (old target 02A)
+-- 002 : ACA00013; --BEQZ  R5, 03A
 -- 003 : 20A50001; --SUBI  R5, R5, 1
--- 004 : ACA00012; --BEQZ  R5, 039   (old target 02A)
+-- 004 : ACA00013; --BEQZ  R5, 03A
 -- 005 : 0C610000; --ADD   R3, R1, R0
 
 -- 006 : 0C850000; --ADD   R4, R5, R0
--- 007 : 00000000;
--- 007 : 20A50001; --SUBI  R5, R5, 1
--- 008 : ACA00014; --BEQZ  R5, 03E   (old target 02F)
--- 009 : 0C430000; --ADD   R2, R3, R0
+-- 007 : 200D0050; --NOP
+-- 008 : 20A50001; --SUBI  R5, R5, 1
+-- 009 : ACA00015; --BEQZ  R5, 03F
+-- 00A : 0C430000; --ADD   R2, R3, R0
 
--- 00A : BC00000C; --JAL   027       (old target 01B)
--- 00B : B4000006; --J     015       (old target 00C)
+-- 00B : BC00000D; --JAL   028
+-- 00C : B4000006; --J     016
 
--- 00C : 0C631000; --ADD   R3, R3, R2
--- 00D : 20840002; --SUBI  R4, R4, 2
--- 00E : B0800010; --BNEZ  R4, 034   (old target 025)
--- 00F : BBE00000; --JR    R31
+-- 00D : 0C631000; --ADD   R3, R3, R2
+-- 00E : 20840002; --SUBI  R4, R4, 2
+-- 00F : B0800011; --BNEZ  R4, 035
+-- 010 : BBE00000; --JR    R31
 
--- 010 : 10840001; --ADDI  R4, R4, 1
--- 011 : B400000C; --J     027       (old target 01B)
+-- 011 : 10840001; --ADDI  R4, R4, 1
+-- 012 : B400000D; --J     028
 
--- 012 : 10600001; --ADDI  R3, R0, 1
--- 013 : B4000014; --J     03E       (old target 02F)
+-- 013 : 10600001; --ADDI  R3, R0, 1
+-- 014 : B4000015; --J     03F
 
--- 014 : 08600000; --SW    f(R0), R3
+-- 015 : 08600000; --SW    f(R0), R3
 
--- 015 : 04200000; --LW    R1, f(R0)
+-- 016 : 04200000; --LW    R1, f(R0)
 
 -- END;
